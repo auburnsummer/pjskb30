6b9ade9986f4224b5e192ac39f181dc7b8b29954
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-07-02 22:04:15 +1000

    cline decided to redesign this as well i guess

--- src/views/ImageView.tsx
+++ src/views/ImageView.tsx
@@ -17,34 +17,55 @@ export function ImageView() {
     const $currSticker = useSignal('/stickers/airi_aim_for_the_top.png');
 
     return (
-        <div class="container is-flex is-flex-direction-column is-align-items-center">
-            <div class="pb-2 pt-2 is-flex is-flex-direction-row is-justify-content-space-between is-gap-1">
-                <div class="is-flex is-flex-direction-row">
-                    <select
-                        class="select is-small"
-                        onChange={e => $currSticker.value = (e.target as HTMLSelectElement).value}
-                    >
-                    {
-                        objectKeys(STICKERS).map(key => (
-                            <option value={STICKERS[key]}>{key}</option>
-                        ))
-                    }
-                    </select>
-                    <button class="button is-small" onClick={() => addSticker($currSticker.value)}>Add sticker</button>
+        <div class="container image-view-container">
+            <div class="image-controls-toolbar">
+                {/* Sticker Controls */}
+                <div class="sticker-controls">
+                    <label for="sticker-select" class="control-label">Add Sticker:</label>
+                    <div class="sticker-input-group">
+                        <select
+                            id="sticker-select"
+                            class="select is-small"
+                            onChange={e => $currSticker.value = (e.target as HTMLSelectElement).value}
+                        >
+                        {
+                            objectKeys(STICKERS).map(key => (
+                                <option value={STICKERS[key]}>{key}</option>
+                            ))
+                        }
+                        </select>
+                        <button class="button is-small" onClick={() => addSticker($currSticker.value)}>Add</button>
+                    </div>
                 </div>
-                <button class="button is-small" onClick={removeAllStickers}>Remove all stickers</button>
-                <button class="button is-small" onClick={changeBackground}>Change background</button>
-                <button class="button is-small is-primary" onClick={downloadImage}>Download image</button>
-                <button class="button is-small is-primary" onClick={copyImageToClipboard}>Copy image to clipboard</button>
-                <button class="button is-small" onClick={() => backToMainView($view)}>Back to main view</button>
+
+                {/* Image Controls */}
+                <div class="image-controls">
+                    <button class="button is-small" onClick={removeAllStickers}>Remove all stickers</button>
+                    <button class="button is-small" onClick={changeBackground}>Change background</button>
+                </div>
+
+                {/* Export Controls */}
+                <div class="export-controls">
+                    <button class="button is-small is-primary" onClick={downloadImage}>Download</button>
+                    <button class="button is-small is-primary" onClick={copyImageToClipboard}>Copy</button>
+                </div>
+
+                {/* Navigation */}
+                <div class="navigation-controls">
+                    <button class="button is-small" onClick={() => backToMainView($view)}>← Back to Data Entry</button>
+                </div>
             </div>
-            <div
-                id="konva-container"
-                onContextMenu={e => {
-                    e.preventDefault();
-                    alert('Use the Download Image or Copy Image to Clipboard buttons instead of right clicking. The reason is because this isn\'t actually a real image, it\'s three canvases in a trenchcoat');
-                }}
-            >
+
+            <div class="canvas-container">
+                <div
+                    id="konva-container"
+                    class="konva-wrapper"
+                    onContextMenu={e => {
+                        e.preventDefault();
+                        alert('Use the Download Image or Copy Image to Clipboard buttons instead of right clicking. The reason is because this isn\'t actually a real image, it\'s three canvases in a trenchcoat');
+                    }}
+                >
+                </div>
             </div>
         </div>
     )
